6b3d3f8cffcfef27bcd71c0834aba544f8bbd31c
sujaym18-blip <kumars302003@gmail.com>
2025-10-03 20:41:25 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        auto: 'repeat(auto-fit, minmax(240px, 1fr))',
+      },
+      fontFamily: {
+        outfit:["Outfit", "sans-serif"],
+        ovo:["Ovo", "serif"],
+      },
+      colors:{
+        darkTheme: '#11001F'
+      },
+    },
+  },
+  darkMode: 'selector'
+};
